3598419b2cbb2ca6e1a3ca5147e31af3855e600a
Olivier Pimi <pimium@gmail.com>
2019-03-10 06:35:01 +0100

    remove not used functions

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 //              ------
 // ----------------------------------------------------------------------------
 
-#define MAX_PWM 0x28
+#define MAX_PWM 0x20
 
 static inline void initTimer0(void)
 {
@@ -27,9 +27,32 @@ static inline void initTimer0(void)
   // Set to 'Fast PWM' mode
   TCCR0A |= (1 << WGM01) | (1 << WGM00);
 
-  OCR0B = MAX_PWM/4+MAX_PWM/8;  // Filament setting
+  OCR0B = MAX_PWM >> 1; //+MAX_PWM/8;  // Filament setting
   OCR0A = MAX_PWM;
 }
+
+//void adc_setup (void)
+//{
+//    // Set the ADC input to PB2/ADC1
+//    ADMUX |= (1 << MUX0);
+//    ADMUX |= (1 << ADLAR);
+//
+//    // Set the prescaler to clock/128 & enable ADC
+//    ADCSRA |= (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);
+//    OCR0B = MAX_PWM/2;
+//    OCR0A = MAX_PWM;
+//}
+//
+//int adc_read (void)
+//{
+//    // Start the conversion
+//    ADCSRA |= (1 << ADSC);
+//
+//    // Wait for it to finish
+//    while (ADCSRA & (1 << ADSC));
+//
+//    return ADCH;
+//}
 
 int main(void)
 {
@@ -39,10 +62,12 @@ int main(void)
   DDRB |= (1 << PB0) | (1 << PB1);
 
   initTimer0();
+//  adc_setup();
 
   sei();
 
   // ---- Main Loop ----
+//  int adc_value;
 
   while (1)
   {
@@ -50,7 +75,9 @@ int main(void)
       PORTB &= ~(1 << PB0);  // High voltage
 //      PORTB &= ~(1 << PB0);  // High voltage
       PORTB |= (1 << PB0); // High Voltage
-      PORTB |= (1 << PB0); // High Voltage
+//      adc_value = adc_read();
+//      OCR0B = adc_value >> 1;
+//      OCR0A = adc_value;
       PORTB |= (1 << PB0); // High Voltage
       PORTB |= (1 << PB0); // High Voltage
       PORTB |= (1 << PB0); // High Voltage
