aaae71bd510740c46a936494e1bae5a8b3ea03af
Olivier Pimi <pimium@gmail.com>
2019-04-08 06:48:46 +0200

    add testcases to the projects - test segment positions - test decimal value.

---
+++ test/Testvfd.c
@@ -0,0 +1,190 @@
+
+#include "vfd.h"
+#include "vfd_test.h"
+#include <unity.h>
+
+/* sometimes you may want to get at local data in a module.
+ * for example: If you plan to pass by reference, this could be useful
+ * however, it should often be avoided */
+
+void setUp(void)
+{
+    /* This is run before EACH TEST */
+    set_register_value(21, 22, 23);
+}
+
+void tearDown(void) {}
+
+void test_InitFunction_dummy(void)
+{
+    uint8_t register_value[3];
+    vfd_init();
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL(21, register_value[0]);
+    TEST_ASSERT_EQUAL(22, register_value[1]);
+    TEST_ASSERT_EQUAL(23, register_value[2]);
+}
+
+// write the decimal 0 to the display: {0x80, 0x6D}, // 0
+void test_vfd_write_word_for_decimal_0(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(2, 0);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL_HEX8(0x6D, 0x6D & register_value[0]);
+    TEST_ASSERT_EQUAL_HEX8(0x80, 0x80 & register_value[1]);
+    TEST_ASSERT_EQUAL_HEX8(0x00, register_value[2]);
+}
+
+// write the decimal 3 to the display: {0x20, 0x6C}, // 3
+void test_vfd_write_word_for_decimal_3(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(0, 3);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL_HEX8(0x6C, 0x6C & register_value[0]);
+    TEST_ASSERT_EQUAL_HEX8(0x20, 0x20 & register_value[1]);
+    TEST_ASSERT_EQUAL_HEX8(0x04, register_value[2]);
+}
+
+// write the decimal 9 to the display: {0xA0, 0x6C}, // 9
+void test_vfd_write_word_for_decimal_9(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(0, 9);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL_HEX8(0x6C, 0x6C & register_value[0]);
+    TEST_ASSERT_EQUAL_HEX8(0xA0, 0xA0 & register_value[1]);
+    TEST_ASSERT_EQUAL_HEX8(0x04, register_value[2]);
+}
+
+void test_vfd_write_word_for_blanking(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(0, 10);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL_HEX8(0x00, register_value[0]);
+    TEST_ASSERT_EQUAL_HEX8(0x00, register_value[1]);
+    TEST_ASSERT_EQUAL_HEX8(0x04, register_value[2]);
+}
+
+void test_vfd_write_word_for_position_0(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(0, 0);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL(0x4, 0x4 & register_value[2]);
+}
+
+void test_vfd_write_word_for_position_1(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(1, 9);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL(0x20, 0x20 & register_value[2]);
+}
+
+void test_vfd_write_word_for_position_2(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(2, 8);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL(0x01, 0x01 & register_value[1]);
+}
+
+void test_vfd_write_word_for_position_3(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(3, 6);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL(0x10, 0x10 & register_value[1]);
+}
+
+void test_vfd_write_word_for_position_4(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(4, 5);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL(0x40, 0x40 & register_value[1]);
+}
+
+void test_vfd_write_word_for_position_5(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(5, 4);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL(0x02, 0x02 & register_value[0]);
+}
+
+void test_vfd_write_word_for_position_6(void)
+{
+    uint8_t register_value[3];
+    vfd_write_word(6, 3);
+
+    get_register_value(register_value);
+    TEST_ASSERT_EQUAL(0x10, 0x10 & register_value[0]);
+}
+
+// void
+// test_FindFunction_WhichIsBroken_ShouldReturnZeroIfItemIsNotInList_WhichWorksEvenInOurBrokenCode(void)
+// {
+//   /* All of these should pass */
+//   TEST_ASSERT_EQUAL(0, FindFunction_WhichIsBroken(78));
+//   TEST_ASSERT_EQUAL(0, FindFunction_WhichIsBroken(2));
+//   TEST_ASSERT_EQUAL(0, FindFunction_WhichIsBroken(33));
+//   TEST_ASSERT_EQUAL(0, FindFunction_WhichIsBroken(999));
+//   TEST_ASSERT_EQUAL(0, FindFunction_WhichIsBroken(-1));
+// }
+
+// void
+// test_FindFunction_WhichIsBroken_ShouldReturnTheIndexForItemsInList_WhichWillFailBecauseOurFunctionUnderTestIsBroken(void)
+// {
+//   /* You should see this line fail in your test summary */
+//   TEST_ASSERT_EQUAL(1, FindFunction_WhichIsBroken(34));
+
+//   /* Notice the rest of these didn't get a chance to run because the line
+//   above failed.
+//    * Unit tests abort each test function on the first sign of trouble.
+//    * Then NEXT test function runs as normal. */
+//   TEST_ASSERT_EQUAL(8, FindFunction_WhichIsBroken(888));
+// }
+
+// void
+// test_FunctionWhichReturnsLocalVariable_ShouldReturnTheCurrentCounterValue(void)
+// {
+//     /* This should be true because setUp set this up for us before this test
+//     */
+//     TEST_ASSERT_EQUAL_HEX(Counter, FunctionWhichReturnsLocalVariable());
+
+//     /* This should be true because we can still change our answer */
+//     Counter = 0x1234;
+//     TEST_ASSERT_EQUAL_HEX(0x1234, FunctionWhichReturnsLocalVariable());
+// }
+
+// void
+// test_FunctionWhichReturnsLocalVariable_ShouldReturnTheCurrentCounterValueAgain(void)
+// {
+//     /* This should be true again because setup was rerun before this test
+//     (and after we changed it to 0x1234) */
+//     TEST_ASSERT_EQUAL_HEX(0x5a5a, FunctionWhichReturnsLocalVariable());
+// }
+
+// void
+// test_FunctionWhichReturnsLocalVariable_ShouldReturnCurrentCounter_ButFailsBecauseThisTestIsActuallyFlawed(void)
+// {
+//     /* Sometimes you get the test wrong.  When that happens, you get a
+//     failure too... and a quick look should tell
+//      * you what actually happened...which in this case was a failure to setup
+//      the initial condition. */
+//     TEST_ASSERT_EQUAL_HEX(Counter, FunctionWhichReturnsLocalVariable());
+// }
--- vfd/vfd.c
+++ vfd/vfd.c
@@ -2,45 +2,66 @@
 // Created by pimi on 29.03.19.
 //
 
-#include <stdint.h>
 #include "vfd.h"
 #include <avr/pgmspace.h>
+#include <stdint.h>
 
 #define MESSAGE_LENGTH 8
 
-const uint8_t decimal_list[][2] PROGMEM = {{0x80, 0x6D}, // 0
-                                           {0x00, 0x28}, // 1
-                                           {0x20, 0x65}, // 2
-                                           {0x20, 0x6C}, // 3
-                                           {0xA0, 0x28}, // 4
-                                           {0xA0, 0x4C}, // 5
-                                           {0xA0, 0x4D}, // 6
-                                           {0x00, 0x68}, // 7
-                                           {0xA0, 0x6D}, // 8
-                                           {0xA0, 0x6C}, // 9
-                                           {0,    0}}; // Nothing
+static uint8_t register_value[3] = {0, 0, 0};
 
-const uint8_t position_list[][2] = {{0x02, 0x04}, // G7
-                                    {0x02, 0x20}, // G6
-                                    {0x01, 0x01}, // G5
-                                    {0x01, 0x10}, // G4
-                                    {0x01, 0x40}, // G3
-                                    {0x00, 0x02}, // G2
-                                    {0x00, 0x10}, // G1
+const uint8_t decimal_list[][2] PROGMEM = {
+    {0x80, 0x6D}, // 0
+    {0x00, 0x28}, // 1
+    {0x20, 0x65}, // 2
+    {0x20, 0x6C}, // 3
+    {0xA0, 0x28}, // 4
+    {0xA0, 0x4C}, // 5
+    {0xA0, 0x4D}, // 6
+    {0x00, 0x68}, // 7
+    {0xA0, 0x6D}, // 8
+    {0xA0, 0x6C}, // 9
+    {0, 0}        // Blank
 };
-const uint8_t symbole_list[][2] = {{0x21, 0x80}, //Glocke
-                                   {0x22, 0x40}, // Ofen
-                                   {0x22, 0x08}, // D
-                                   {0x10, 0x08}, // A am
-                                   {0x10, 0x04}, // P pm
-                                   {0x10, 0x02}, // Auto
-                                   {0x22, 0x10}, // :
-                                   {0x23, 0x08}, // Stop
-                                   {0x24, 0x02}, // Kolben
-                                   {0x25, 0x01}, // Kasten
-                                   {0x26, 0x08}, // Start
-                                   {0x06, 0x80}, // Celcius
+
+const uint8_t position_list[][2] = {
+    {0x02, 0x04}, // G7
+    {0x02, 0x20}, // G6
+    {0x01, 0x01}, // G5
+    {0x01, 0x10}, // G4
+    {0x01, 0x40}, // G3
+    {0x00, 0x02}, // G2
+    {0x00, 0x10}  // G1
 };
+
+const uint8_t symbole_list[][2] = {
+    {0x21, 0x80}, // Glocke
+    {0x22, 0x40}, // Ofen
+    {0x22, 0x08}, // D
+    {0x10, 0x08}, // A am
+    {0x10, 0x04}, // P pm
+    {0x10, 0x02}, // Auto
+    {0x22, 0x10}, // :
+    {0x23, 0x08}, // Stop
+    {0x24, 0x02}, // Kolben
+    {0x25, 0x01}, // Kasten
+    {0x26, 0x08}, // Start
+    {0x06, 0x80}, // Celcius
+};
+
+void set_register_value(uint8_t value1, uint8_t value2, uint8_t value3)
+{
+    register_value[0] = value1;
+    register_value[1] = value2;
+    register_value[2] = value3;
+}
+
+void get_register_value(uint8_t *reg)
+{
+    reg[0] = register_value[0];
+    reg[1] = register_value[1];
+    reg[2] = register_value[2];
+}
 
 void vfd_init(void)
 {
@@ -75,13 +96,11 @@ void vfd_write_byte(uint8_t byte)
 
 void vfd_write_word(uint8_t pos, uint8_t value)
 {
-    uint8_t register_value[3] = {0, 0, 0};
-
     uint8_t position_value = position_list[pos][1];
     uint8_t position = position_list[pos][0];
 
-    register_value[0] = pgm_read_byte(&decimal_list[value][1]);
-    register_value[1] = pgm_read_byte(&decimal_list[value][0]);
+    register_value[0] = pgm_read_byte(&(decimal_list[value][1]));
+    register_value[1] = pgm_read_byte(&(decimal_list[value][0]));
     register_value[2] = 0;
     register_value[position] = register_value[position] | position_value;
 
@@ -93,10 +112,9 @@ void vfd_write_word(uint8_t pos, uint8_t value)
     PORTB |= (1 << RCLK);
 }
 
+//
 void vfd_write_special_character(uint8_t symb)
 {
-    uint8_t register_value[3] = {0, 0, 0};
-
     uint8_t filament_byte = symbole_list[symb][0];
     uint8_t symb_value = symbole_list[symb][1];
 
