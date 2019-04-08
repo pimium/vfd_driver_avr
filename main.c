b6a6af84b1b4271e311ea4aea4ba5a7b83c251c2
Olivier Pimi <pimium@gmail.com>
2019-04-08 07:16:24 +0200

    remove unused timer functionalities

--- main.c
+++ main.c
@@ -24,35 +24,15 @@
 
 #define MAX_PWM 0x20
 
-static inline void initTimer0(void)
-{
-    // Timer 0 Configuration
-    TCCR0B |= (0 << CS02) | (0 << CS01) | (1 << CS00) // Prescaler = 1
-              | (1 << WGM02);                         // Fast PWM
-
-    TCCR0A |= (0 << COM0A1) | (0 << COM0A0); // Clear PORT OC0A on Top
-    TCCR0A |= (1 << COM0B1) | (0 << COM0B0);
-
-    // Set to 'Fast PWM' mode
-    TCCR0A |= (1 << WGM01) | (1 << WGM00);
-
-    OCR0B = MAX_PWM >> 1; //+MAX_PWM/8;  // Filament setting
-    OCR0A = MAX_PWM;
-}
-
 int main(void)
 {
 
     // ---- Initialization ----
     vfd_init();
-
-    //  initTimer0();
     //  sei();
-    //
 
     // ---- Main Loop ----
 
-    //
     while (1)
     {
 
