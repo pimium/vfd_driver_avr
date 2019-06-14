a6af35d4ee38cf9d5b3aac2862907a0577b72e48
Olivier Pimi <pimium@gmail.com>
2019-06-14 19:39:50 +0200

    states machines for test

--- vfd/vfd_internal.h
+++ vfd/vfd_internal.h
@@ -6,11 +6,13 @@
 #define NUMBER_OF_CHARACTER 7
 #define NUMBER_OF_REGISTER 4
 
+typedef void *(*states)(void);
+//typedef void *(*StateFunc)();
 // States
-void vfd_states_start_write_byte(void);
-void vfd_states_set_srclr(void);
-void vfd_states_set_clk(void);
-void vfd_states_clear_clk(void);
-void vfd_states_set_word(void);
-void vfd_states_set_rclK(void);
+void * vfd_states_start_write_byte(void);
+void * vfd_states_set_srclr(void);
+void * vfd_states_set_clk(void);
+void * vfd_states_clear_clk(void);
+void * vfd_states_set_word(void);
+void * vfd_states_set_rclK(void);
 #endif
